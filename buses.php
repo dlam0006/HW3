f90c6367ee27472eff51c6dd439901fddc678fc3
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-04 12:44:16 -0500

    Update buses.php

--- buses.php
+++ buses.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-buses.php");
+  
+$pageTitle = "Buses";
+include "view-header.php";
+$drivers = selectBuses();
+include "view-footer.php";
+?>
