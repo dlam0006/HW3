d4d2f33d903d22ccd67be18aae0bc3ce81e051da
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-04 11:43:00 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $conn = new mysqli('159.89.47.44', 'dlaminio_HW3User', 'f[%-^fW[xWx]', 'dlaminio_HW3');
     
     // Check connection
     if ($conn->connect_error) {
