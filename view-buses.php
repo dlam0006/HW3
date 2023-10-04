3213fd257ab5f0371d87558122777b6015953aad
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-04 12:53:20 -0500

    Create view-buses.php

---
+++ view-buses.php
@@ -0,0 +1,25 @@
+<h1>Buses</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Bus_ID</th>
+        <th>Name</th>
+        <th>Capacity</th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php
+      while ($bus = $buses->fetch_assoc()) {
+  ?>
+     <tr>
+       <td><?php echo $bus['bus_id']; ?></td>
+        <td><?php echo $bus['name']; ?></td>
+       <td><?php echo $bus['capacity']; ?></td>
+     </tr>     
+  <?php
+      }
+  ?> 
+    </tbody>
+  </table>
+</div>
