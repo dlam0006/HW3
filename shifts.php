90888f4c2d67b0ec98e5e3e904789e59592ac867
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-04 12:45:24 -0500

    Create shifts.php

---
+++ shifts.php
@@ -0,0 +1,9 @@
+<?php
+require_once("util-db.php");
+require_once("model-shifts.php");
+  
+$pageTitle = "Shifts";
+include "view-header.php";
+$drivers = selectShifts();
+include "view-footer.php";
+?>
