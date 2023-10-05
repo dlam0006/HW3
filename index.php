69b585442cc3259947481d54b81041eb4426947d
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-05 12:00:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,11 @@ include "view-header.php";
 ?>
     <br/>
     <h1>Homework Three</h1><br/>
-<p>
-    This is a database for a bus transportation company. It shows drivers and the buses they drive, buses and when they are driven, drivers and their shifts along with the route they take during each shift.
-</p> <br/>
-<p>
-    This database uses the tables: Drivers, Buses, Shifts, and Routes.
-</p>
+<p>This is a database for a bus transportation company.</p> <br/>
+<p>Tab 1 (Drivers): Drivers and the buses they drive (with schedule)</p><br/>
+<p>Tab 2 (Buses): Buses and their working schedule</p><br/>
+<p>Tab 3 (Drivers with Shifts): Divers and their shifts along with the route they take during each shift</p><br/><br/>
+<p>This database uses the tables: Drivers, Buses, Shifts, and Routes.</p>
 <?php
 include "view-footer.php";
 ?>
