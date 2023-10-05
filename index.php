102f8f6bb668e3b8e538ba154d12812800f85bb1
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-05 11:55:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-    <h1>Homework Three</h1>
+    <br/>
+    <h1>Homework Three</h1><br/>
+<p>
+    This is a database for a bus transportation company. It shows drivers and the buses they drive, buses and when they are driven, drivers and their shifts along with the route they take during each shift.
+</p> <br/>
+<p>
+    This database uses the tables: Drivers, Buses, Shifts, and Routes.
+</p>
 <?php
 include "view-footer.php";
 ?>
