1ac1afd186964f021cda85ca6d61ea840601e7e2
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-04 20:39:15 -0500

    Update view-drivers.php

--- view-drivers.php
+++ view-drivers.php
@@ -17,7 +17,7 @@
        <td><?php echo $driver['driver_id']; ?></td>
        <td><?php echo $driver['name']; ?></td>
        <td><?php echo $driver['email']; ?></td>
-       <td><a href="driver-buses.php?id=<?php echo $driver['driver_id']; ?>">Buses</a></td>
+       <td><a href="buses-by-driver.php?id=<?php echo $driver['driver_id']; ?>">Buses</a></td>
      </tr>     
   <?php
   }
