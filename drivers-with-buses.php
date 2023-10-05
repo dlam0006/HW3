517914b32b9ddfb2752cf4982177c3d09b4c3fe0
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-04 22:20:30 -0500

    Update drivers-with-buses.php

--- drivers-with-buses.php
+++ drivers-with-buses.php
@@ -1,1 +1,10 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-drivers-with-buses.php");
+  
+$pageTitle = "Drivers with Buses";
+include "view-header.php";
+$drivers = selectDrivers();
+include "view-drivers-with-buses.php";
+include "view-footer.php";
+?>
