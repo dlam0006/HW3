c19f5ddb411ee42b708b3440d47e006501eda2d4
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-04 22:53:44 -0500

    Update view-drivers-with-buses.php

--- view-drivers-with-buses.php
+++ view-drivers-with-buses.php
@@ -7,14 +7,17 @@
         <div class="card-body">
           <h5 class="card-title"><?php echo $driver['name']; ?></h5>
           <p class="card-text">
+            <ul class="list-group">
             <?php
               $buses = selectBusesByDriver($driver['driver_id']);
               while ($bus = $buses->fetch_assoc()) {
             ?>
-                
+                <li class="list-group-item"><?php echo $bus['bus_name']; ?> - <?php echo $bus['capacity']; ?> - <?php echo $bus['shift_days']; ?> - <?php echo $bus['start_time']; ?> - <?php echo $bus['end_time']; ?></li>
+
             <?php   
               }
            ?>
+            </ul>
           </p>
           <p class="card-text"><small class="text-body-secondary">Email: <?php echo $driver['email']; ?></small></p>
         </div>
