8d09cb428eca05025f96ca9125c8f57fcef3438c
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-23 14:40:07 -0500

    Update model-buses.php

--- model-buses.php
+++ model-buses.php
@@ -12,4 +12,18 @@ function selectBuses() {
         throw $e;
     }
 }
+
+function insertBuses() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT bus_id, bus_name, capacity FROM `Bus`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
