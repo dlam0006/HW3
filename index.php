a0343cd003acfe73dd0e8b3644305dad7b721820
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 06:21:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <br/>
-    <h1>Homework Three</h1><br/>
+    <h1>Homework Four</h1><br/>
 <p>This is a database for a bus transportation company.</p> <br/>
 <p>Tab 1 (Drivers): Drivers and the buses they drive (with schedule)</p>
 <p>Tab 2 (Buses): Buses and their working schedule</p>
