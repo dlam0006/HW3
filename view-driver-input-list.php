e0c9f52169e497e7cb8481644f35a8ab811eced6
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 12:15:13 -0500

    Create view-driver-input-list.php

---
+++ view-driver-input-list.php
@@ -0,0 +1,14 @@
+<select class="form-select" id="did" name="did">
+  <?php
+    while ($DriverItem = $
+  ?>
+
+  <?php
+
+  ?>
+
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
