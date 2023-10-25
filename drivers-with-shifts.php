587cdef91c9757013bd1a4f2319bb51f14f9dd1c
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 05:39:20 -0500

    Update drivers-with-shifts.php

--- drivers-with-shifts.php
+++ drivers-with-shifts.php
@@ -8,6 +8,13 @@ include "view-header.php";
 
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
+        case "Add2":
+            if (insertShifts($_POST['did'], $_POST['bid'], $_POST['rid'], $_POST['sDays'], $_POST['sStart'], $_POST['sEnd'])) {
+                echo '<div class="alert alert-success" role="alert">Shift Added.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
         case "Add":
             if (insertRoutes($_POST['rOrigin'], $_POST['rDestination'])) {
                 echo '<div class="alert alert-success" role="alert">Route Added.</div>';
