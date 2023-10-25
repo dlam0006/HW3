4b07e52e66704726d734dac1309221b109becf9f
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 03:48:37 -0500

    Update view-drivers.php

--- view-drivers.php
+++ view-drivers.php
@@ -35,7 +35,7 @@
        </td>
        <td>
          <form method="post" action="">
-           <input type="hidden" name="did" value="<?php echo $bus['driver_id']; ?>">
+           <input type="hidden" name="did" value="<?php echo $driver['driver_id']; ?>">
            <input type="hidden" name="actionType" value="Delete">
             <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
