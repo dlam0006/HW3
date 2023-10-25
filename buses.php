3b90af2475468c77d82d768af3bb0b790f37e9f9
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 01:59:13 -0500

    Update buses.php

--- buses.php
+++ buses.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
-            if (insertBus($_POST['bName'], $_POST['bCapacity'])) {
+            if (insertBuses($_POST['bName'], $_POST['bCapacity'])) {
                 echo '<div class="alert alert-success" role="alert">Bus Added.</div>';
             } else {
                 echo '<div class="alert alert-danger" role="alert">Error.</div>';
