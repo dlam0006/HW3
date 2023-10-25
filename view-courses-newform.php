3ec99c3ecf217b7230461475d13fe8210fbed855
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 01:18:28 -0500

    Update view-courses-newform.php

--- view-courses-newform.php
+++ view-courses-newform.php
@@ -24,6 +24,7 @@
             <label for="bCapacity" class="form-label">Bus Capacity</label>
             <input type="text" class="form-control" id="bCapacity" name="bCapacity">
           </div>
+            <input type="hidden" name="actionType" value="Add">
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
       </div>
