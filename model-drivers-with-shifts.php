aecaf66dccf4fd8cee0fc4f64a240cda5d6e7c4e
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 05:04:00 -0500

    Update model-drivers-with-shifts.php

--- model-drivers-with-shifts.php
+++ model-drivers-with-shifts.php
@@ -27,4 +27,46 @@ function selectBusesByDriver($did) {
         throw $e;
     }
 }
+
+function insertShifts($did, $bid, $rid, $sDays, $sStart, $sEnd) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Shift` (`driver_id`, `bus_id`, `route_id`, `shift_days`, `start_time`, `end_time` ) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("iiisss", $did, $bid, $rid, $sDays, $sStart, $sEnd);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateShifts($did, $bid, $rid, $sDays, $sStart, $sEnd, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Shift` SET `driver_id` = ?, `bus_id` = ?, `route_id` = ?, `shift_days` = ?, `start_time` = ?, `end_time` = ?  WHERE `Shift`.`shift_id` = ?");
+        $stmt->bind_param("iiisssi", $did, $bid, $rid, $sDays, $sStart, $sEnd, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteShifts($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `Shift` where shift_id = ?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
