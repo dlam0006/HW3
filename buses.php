aad5c7551f002433014e6a039cf18c39ad9b9dc9
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 01:55:47 -0500

    Update buses.php

--- buses.php
+++ buses.php
@@ -4,6 +4,19 @@ require_once("model-buses.php");
   
 $pageTitle = "Buses";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertBus($_POST['bName'], $_POST['bCapacity'])) {
+                echo '<div class="alert alert-success" role="alert">Bus Added.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+    }
+}
+
 $buses = selectBuses();
 include "view-buses.php";
 include "view-footer.php";
