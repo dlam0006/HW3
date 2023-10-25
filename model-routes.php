93fc339bd271a5b365ebf2d1c9ba779b3c5798fd
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 04:06:40 -0500

    Update model-routes.php

--- model-routes.php
+++ model-routes.php
@@ -12,4 +12,46 @@ function selectRoutes() {
         throw $e;
     }
 }
+
+function insertRoutes($rOrigin, $rDestination) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Route` (`origin`, `destination`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $rOrigin, $rDestination);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateRoutes($rOrigin, $rDestination, $rid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Route` SET `origin` = ?, `destination` = ? WHERE `Route`.`route_id` = ?");
+        $stmt->bind_param("ssi", $rOrigin, $rDestination, $rid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteRoutes($rid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `Route` where route_id = ?");
+        $stmt->bind_param("i", $rid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
