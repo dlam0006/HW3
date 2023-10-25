aafb973f6524ea0991ec007faa11597570785266
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-10-25 04:28:06 -0500

    Update drivers-with-shifts.php

--- drivers-with-shifts.php
+++ drivers-with-shifts.php
@@ -6,6 +6,34 @@ require_once("model-routes.php");
 $pageTitle = "Drivers with Shifts";
 include "view-header.php";
 $drivers = selectDrivers();
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertRoutes($_POST['rOrigin'], $_POST['rDestination'])) {
+                echo '<div class="alert alert-success" role="alert">Route Added.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+        case "Edit":
+            if (updateRoutes($_POST['rOrigin'], $_POST['rDestination'], $_POST['rid'])) {
+                echo '<div class="alert alert-success" role="alert">Route Edited.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+        case "Delete":
+            if (deleteRoutes($_POST['rid'])) {
+                echo '<div class="alert alert-success" role="alert">Route Deleted.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error.</div>';
+            }
+            break;
+    }
+}
+
+
 $routes = selectRoutes();
 include "view-drivers-with-shifts.php";
 include "view-footer.php";
