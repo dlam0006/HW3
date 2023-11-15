18180e043f66625d3610b2dfb2b5daa6f9a43f1d
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-11-14 23:11:29 -0600

    Update view-visualizations.php

--- view-visualizations.php
+++ view-visualizations.php
@@ -1,8 +1,38 @@
-<h1>Drivers</h1>
+<h1>Analysis Visualizations</h1>
 
-  <?php
-      while ($driver = $drivers->fetch_assoc()) {
-        
-  }
-  ?> 
    
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+ 
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+            <?php
+                while ($driver = $drivers->fetch_assoc()) {
+                  echo $driver['num_shifts'] . ", ";
+            }
+            ?>
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+            <?php
+            $drivers = selectDrivers();
+                while ($driver = $drivers->fetch_assoc()) {
+                  echo "'" . $driver['name'] . "', ";
+            }
+            ?>
+    ]
+}
+  });
+</script>
