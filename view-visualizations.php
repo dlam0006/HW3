2c7801fce4d36af335ecc05bcd831620bccf710e
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-11-15 10:56:28 -0600

    Update view-visualizations.php

--- view-visualizations.php
+++ view-visualizations.php
@@ -36,87 +36,85 @@
 
 </script>
 
-<div style="width: 600px; height: 600px;">
-  <canvas id="myChart2"></canvas>
-</div>
 
-<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<!-- Create a container for the chart -->
+<div id="chart"></div>
+
+<!-- Include the ApexCharts JavaScript file -->
+<script src="https://cdn.jsdelivr.net/npm/apexcharts@3.26.0/dist/apexcharts.min.js"></script>
 
 <script>
-     const ctx2 = document.getElementById('myChart2');
-      
-        var options = {
-          series: [{
-          name: 'Servings',
-          data: [44, 55, 41, 67, 22, 43, 21, 33, 45, 31, 87, 65, 35]
+    // Your chart options and rendering code
+    var options = {
+        series: [{
+            name: 'Servings',
+            data: [44, 55, 41, 67, 22, 43, 21, 33, 45, 31, 87, 65, 35]
         }],
-          annotations: {
-          points: [{
-            x: 'Bananas',
-            seriesIndex: 0,
-            label: {
-              borderColor: '#775DD0',
-              offsetY: 0,
-              style: {
-                color: '#fff',
-                background: '#775DD0',
-              },
-              text: 'Bananas are good',
-            }
-          }]
+        annotations: {
+            points: [{
+                x: 'Bananas',
+                seriesIndex: 0,
+                label: {
+                    borderColor: '#775DD0',
+                    offsetY: 0,
+                    style: {
+                        color: '#fff',
+                        background: '#775DD0',
+                    },
+                    text: 'Bananas are good',
+                }
+            }]
         },
         chart: {
-          height: 350,
-          type: 'bar',
+            height: 350,
+            type: 'bar',
         },
         plotOptions: {
-          bar: {
-            borderRadius: 10,
-            columnWidth: '50%',
-          }
+            bar: {
+                borderRadius: 10,
+                columnWidth: '50%',
+            }
         },
         dataLabels: {
-          enabled: false
+            enabled: false
         },
         stroke: {
-          width: 2
+            width: 2
         },
-        
         grid: {
-          row: {
-            colors: ['#fff', '#f2f2f2']
-          }
+            row: {
+                colors: ['#fff', '#f2f2f2']
+            }
         },
         xaxis: {
-          labels: {
-            rotate: -45
-          },
-          categories: ['Apples', 'Oranges', 'Strawberries', 'Pineapples', 'Mangoes', 'Bananas',
-            'Blackberries', 'Pears', 'Watermelons', 'Cherries', 'Pomegranates', 'Tangerines', 'Papayas'
-          ],
-          tickPlacement: 'on'
+            labels: {
+                rotate: -45
+            },
+            categories: ['Apples', 'Oranges', 'Strawberries', 'Pineapples', 'Mangoes', 'Bananas',
+                'Blackberries', 'Pears', 'Watermelons', 'Cherries', 'Pomegranates', 'Tangerines', 'Papayas'
+            ],
+            tickPlacement: 'on'
         },
         yaxis: {
-          title: {
-            text: 'Servings',
-          },
+            title: {
+                text: 'Servings',
+            },
         },
         fill: {
-          type: 'gradient',
-          gradient: {
-            shade: 'light',
-            type: "horizontal",
-            shadeIntensity: 0.25,
-            gradientToColors: undefined,
-            inverseColors: true,
-            opacityFrom: 0.85,
-            opacityTo: 0.85,
-            stops: [50, 0, 100]
-          },
+            type: 'gradient',
+            gradient: {
+                shade: 'light',
+                type: "horizontal",
+                shadeIntensity: 0.25,
+                gradientToColors: undefined,
+                inverseColors: true,
+                opacityFrom: 0.85,
+                opacityTo: 0.85,
+                stops: [50, 0, 100]
+            },
         }
-        };
+    };
 
-        var chart = new ApexCharts(ctx2, options);
-        chart.render();
-      
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
 </script>
