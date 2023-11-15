06be96a129ba3ba439b149a95b9dc947af22fb9e
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-11-15 00:31:48 -0600

    Update model-visualizations-db.php

--- model-visualizations-db.php
+++ model-visualizations-db.php
@@ -12,4 +12,18 @@ function selectDrivers() {
         throw $e;
     }
 }
+
+function selectBuses() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT bus_name, count(s.shift_id) as num_shifts FROM `Bus` b join `Shift` s on b.bus_id = s.bus_id group by bus_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
