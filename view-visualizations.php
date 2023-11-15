b1c09eb2eaf3bdd112f2ed6cc076847bf884534c
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-11-15 12:50:51 -0600

    Update view-visualizations.php

--- view-visualizations.php
+++ view-visualizations.php
@@ -1,145 +1,6 @@
 <h1>Analysis Visualizations</h1>
-  
-<div style="width: 300px; height: 300px;">
-  <canvas id="myChart"></canvas>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
- 
-<script>
-  const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-    datasets: [{
-        data: [
-            <?php
-                while ($driver = $drivers->fetch_assoc()) {
-                  echo $driver['num_shifts'] . ", ";
-            }
-            ?>
-        ]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-            <?php
-            $drivers = selectDrivers();
-                while ($driver = $drivers->fetch_assoc()) {
-                  echo "'" . $driver['name'] . "', ";
-            }
-            ?>
-    ]
-}
-  });
-
-</script>
 
 
-<div id="chart"></div>
-
-<script src="https://cdn.jsdelivr.net/npm/apexcharts@3.26.0/dist/apexcharts.min.js"></script>
-
-<script>
-    var options = {
-        series: [{
-            name: 'Number of Shifts',
-            data: [
-              <?php
-                while ($bus = $buses->fetch_assoc()) {
-                  echo $bus['num_shifts'] . ", ";
-            }
-            ?>
-            ]
-        }],
-        chart: {
-            height: 350,
-            type: 'bar',
-        },
-        plotOptions: {
-            bar: {
-                borderRadius: 10,
-                columnWidth: '50%',
-            }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            width: 2
-        },
-        grid: {
-            row: {
-                colors: ['#fff', '#f2f2f2']
-            }
-        },
-        xaxis: {
-            labels: {
-                rotate: -45
-            },
-            categories: [
-            <?php
-            $buses = selectBuses();
-                while ($bus = $buses->fetch_assoc()) {
-                  echo "'" . $bus['bus_name'] . "', ";
-            }
-            ?>
-            ],
-            tickPlacement: 'on'
-        },
-        yaxis: {
-            title: {
-                text: 'Number of Shifts',
-            },
-          tickAmount: 2
-        },
-        fill: {
-            type: 'gradient',
-            gradient: {
-                shade: 'light',
-                type: "horizontal",
-                shadeIntensity: 0.25,
-                gradientToColors: undefined,
-                inverseColors: true,
-                opacityFrom: 0.85,
-                opacityTo: 0.85,
-                stops: [50, 0, 100]
-            },
-        }
-    };
-
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
-</script>
-
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
-
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Route', 'Route Count'],
-          <?php
-                while ($route = $routes->fetch_assoc()) {
-                  echo "['Route " . $route['route_id'] . "', " . $route['route_count'] . "], ";
-            }
-            ?>
-        ]);
-
-        var options = {
-          title: 'Routes Count'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
-    </script>
 
     <!-- Include Highcharts library -->
     <script src="https://code.highcharts.com/highcharts.js"></script>
@@ -321,4 +182,146 @@ Highcharts.chart('container', {
         }]
     }]
 });
+    </script>
+
+  
+<div style="width: 300px; height: 300px;">
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+ 
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+            <?php
+                while ($driver = $drivers->fetch_assoc()) {
+                  echo $driver['num_shifts'] . ", ";
+            }
+            ?>
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+            <?php
+            $drivers = selectDrivers();
+                while ($driver = $drivers->fetch_assoc()) {
+                  echo "'" . $driver['name'] . "', ";
+            }
+            ?>
+    ]
+}
+  });
+
+</script>
+
+
+<div id="chart"></div>
+
+<script src="https://cdn.jsdelivr.net/npm/apexcharts@3.26.0/dist/apexcharts.min.js"></script>
+
+<script>
+    var options = {
+        series: [{
+            name: 'Number of Shifts',
+            data: [
+              <?php
+                while ($bus = $buses->fetch_assoc()) {
+                  echo $bus['num_shifts'] . ", ";
+            }
+            ?>
+            ]
+        }],
+        chart: {
+            height: 350,
+            type: 'bar',
+        },
+        plotOptions: {
+            bar: {
+                borderRadius: 10,
+                columnWidth: '50%',
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            width: 2
+        },
+        grid: {
+            row: {
+                colors: ['#fff', '#f2f2f2']
+            }
+        },
+        xaxis: {
+            labels: {
+                rotate: -45
+            },
+            categories: [
+            <?php
+            $buses = selectBuses();
+                while ($bus = $buses->fetch_assoc()) {
+                  echo "'" . $bus['bus_name'] . "', ";
+            }
+            ?>
+            ],
+            tickPlacement: 'on'
+        },
+        yaxis: {
+            title: {
+                text: 'Number of Shifts',
+            },
+          tickAmount: 2
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shade: 'light',
+                type: "horizontal",
+                shadeIntensity: 0.25,
+                gradientToColors: undefined,
+                inverseColors: true,
+                opacityFrom: 0.85,
+                opacityTo: 0.85,
+                stops: [50, 0, 100]
+            },
+        }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+</script>
+
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Route', 'Route Count'],
+          <?php
+                while ($route = $routes->fetch_assoc()) {
+                  echo "['Route " . $route['route_id'] . "', " . $route['route_count'] . "], ";
+            }
+            ?>
+        ]);
+
+        var options = {
+          title: 'Routes Count'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
     </script>
