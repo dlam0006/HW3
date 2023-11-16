c268e34e2979d30bfc306cc47bdea4f985805565
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-11-15 22:39:02 -0600

    Update view-visualizations.php

--- view-visualizations.php
+++ view-visualizations.php
@@ -127,11 +127,9 @@
     var chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
 </script>
-
-
 <div class="chart-container">
-   <h3>Number of Shifts for each Drive per Week</h3>
     <div style="width: 600px; height: 600px;">
+      <h3>Number of Shifts for each Drive per Week</h3>
       <canvas id="myChart"></canvas>
     </div>
     <h3>Route Count per Week</h3>
