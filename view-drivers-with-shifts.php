9d98c2944d09d10d0ed3188a7605459d89c916e3
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-12-06 01:05:09 -0600

    Update view-drivers-with-shifts.php

--- view-drivers-with-shifts.php
+++ view-drivers-with-shifts.php
@@ -115,3 +115,61 @@
     </tbody>
   </table>
 </div>
+
+<br></br>
+
+<h1>Bus Route Calculator</h1>
+
+    <label for="selectRoute">Select Route:</label>
+    <select id="selectRoute">
+        <option value="20">Route 13 (20 miles)</option>
+        <option value="17">Route 14 (17 miles)</option>
+        <option value="33">Route 15 (33 miles)</option>
+        <option value="27">Route 16 (13 miles)</option>
+        <option value="13">Route 17 (27 miles)</option>
+    </select>
+
+    <br>
+
+    <button onclick="calculateRoutesBeforeRefuel()">Calculate Routes Before Refuel</button>
+
+    <p id="results"></p>
+
+    <script>
+        function calculateRoutesBeforeRefuel() {
+            const selectRoute = document.getElementById('selectRoute');
+            const selectedRouteMiles = parseInt(selectRoute.value);
+
+            const milesPerTank = 300; // Assuming the bus can cover 300 miles per tank
+
+            const routesBeforeRefuel = Math.floor(milesPerTank / selectedRouteMiles);
+
+            const resultsParagraph = document.getElementById('results');
+            resultsParagraph.textContent = `Number of routes before needing to refuel: ${routesBeforeRefuel}`;
+        }
+    </script>
+
+<br></br>
+
+<div id="map"></div>
+<h3>Map of Route</h3>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
+    <script>
+        const map = L.map('map').setView([35.2226, -97.4395], 13); // Set your initial coordinates and zoom level
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+
+        // Example bus route coordinates (replace with your actual route coordinates)
+        const busRoute = [
+            [35.2226, -97.4395, "Start Location - Norman, OK"],
+            [35.3395, -97.4867, "Moore, OK"],
+            [35.4676, -97.5164, "Oklahoma City, OK"],
+                                    [35.6528, -97.4781, "Edmund, OK"],
+            [35.2226, -97.4395, "End Location - Norman, OK"]
+        ];
+
+        L.polyline(busRoute, { color: 'blue' }).addTo(map);
+    </script>
