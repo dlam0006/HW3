653644f8da3b28acf62f72e9c511d870a9741372
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-12-05 18:38:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -49,14 +49,14 @@ include "view-header.php";
         <p>This platform serves as a comprehensive database for a bus transportation company, facilitating efficient resource planning and management.</p>
         <p>This database comprises several tabs, each dedicated to a specific aspect of the transportation operations:</p>
 
-        <ol>
+        <ul>
             <li><strong>Drivers:</strong> Displays information about drivers, the buses they operate, and their schedules.</li>
             <li><strong>Buses:</strong> Provides details about buses and their working schedules.</li>
             <li><strong>Drivers with Shifts:</strong> Shows drivers, their shifts, and the routes they take during each shift.</li>
             <li><strong>Employees Weekly Pay:</strong> Calculates the hours each employee works per week and determines their pay.</li>
             <li><strong>Routes Driven Before Refueling:</strong> Estimates the number of routes a driver can take before needing to refuel.</li>
             <li><strong>Route Map:</strong> Displays an overview map of the stops on the routes.</li>
-        </ol>
+        </ul>
 
     </div>
 </div>
