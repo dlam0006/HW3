2116d3ef12fde693ec4de80f2770df540fd40aca
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-12-05 19:01:32 -0600

    Update shifts-by-bus.php

--- shifts-by-bus.php
+++ shifts-by-bus.php
@@ -4,6 +4,7 @@ require_once("model-shifts-by-bus.php");
   
 $pageTitle = "Shifts by Bus";
 include "view-header.php";
+include "css.php";
 $shifts = selectShiftsByBus($_POST['bid']);
 include "view-shifts-by-bus.php";
 include "view-footer.php";
