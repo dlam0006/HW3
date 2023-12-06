14419ad211177a013206f9eb64d7e60524929670
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-12-06 13:25:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include "css.php";
 <div class="container">
 
     <div style="margin-top: 20px;">
-        <h2>Bellah Buses Resource Planning System</h2>
+        <h2>Bellah Buses Management System</h2>
             <div style="text-align: center; margin-top: 20px;">
                 <img src="Logo.png" alt="Bus Transportation Image">
             </div>
@@ -19,10 +19,8 @@ include "css.php";
         <ul>
             <li><strong>Drivers:</strong> Displays information about drivers, the buses they operate, and their schedules.</li>
             <li><strong>Buses:</strong> Provides details about buses and their working schedules.</li>
-            <li><strong>Drivers with Shifts:</strong> Shows drivers, their shifts, and the routes they take during each shift.</li>
-            <li><strong>Employees Weekly Pay:</strong> Calculates the hours each employee works per week and determines their pay.</li>
-            <li><strong>Routes & Refueling:</strong> Estimates the number of routes a driver can take before needing to refuel.</li>
-            <li><strong>Route Map:</strong> Displays an overview map of the stops on the routes.</li>
+            <li><strong>Shifts & Routes:</strong> Shows shift information and calculate costs.</li>
+            <li><strong>Overview:</strong> Shows visualizations for resource usage.</li>
         </ul>
 
     </div>
