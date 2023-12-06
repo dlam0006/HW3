1a4d2e806558ef37b6d08479daf2852cd1ed8ee6
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-12-05 18:34:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include "view-header.php";
     }
 
     img {
-        max-width: 100%;
+        max-width: 150%;
         height: auto;
     }
 
@@ -42,7 +42,7 @@ include "view-header.php";
     <div style="margin-top: 20px;">
         <h2>Bellah Buses Resource Planning System</h2>
             <div style="text-align: center; margin-top: 20px;">
-                <img src="Logo.png" alt="Bus Transportation Image">
+                <img src="Logo_1.png" alt="Bus Transportation Image">
             </div>
         
         <h4>Site Naviagtion</h4>
