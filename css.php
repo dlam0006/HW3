5c3114265f065c6cda35d00f3f40086be338f568
dlam0006 <98052542+dlam0006@users.noreply.github.com>
2023-12-05 18:41:05 -0600

    Create css.php

---
+++ css.php
@@ -0,0 +1,33 @@
+<style>
+    body {
+        background-color: #f0f8ff;
+        font-family: Arial, sans-serif;
+        font-family: 'Montserrat', sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    .container {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+
+    h1, h2, h4 {
+        color: #333;
+        text-align: center;
+    }
+
+    p {
+        color: #555;
+    }
+</style>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap">
